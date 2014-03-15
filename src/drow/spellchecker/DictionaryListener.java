96ca61a2f356bf5aaa26a1bf902a8093297d4913
dartvader <C07042060@mydit.ie>
2014-03-15 00:01:09 +0000

    The dictionary listener is now running in a seperate thread.
    I also add the refernce to the final word to the highlighter.
    
    There is a bug in the highlighter.
    
     "Invalid end offset"

--- src/drow/spellchecker/DictionaryListener.java
+++ src/drow/spellchecker/DictionaryListener.java
@@ -15,6 +15,9 @@ public class DictionaryListener implements KeyListener {
 	
 	JTextPane typingArea;
 	Dictionary d;
+	private static final int BACK_KEY = 8;
+	private static final int MAX_SIZE_WORD = 20; // only pull 20 chars off text pane to save on splitting the entire string
+	private static int CURRENT_MAX_WORD = 0; // starts at zero
 	
     public DrowHighlightManager highlightManager;
 	
@@ -33,36 +36,42 @@ public class DictionaryListener implements KeyListener {
 	@Override
 	public void keyPressed(KeyEvent e) {
 		// TODO Auto-generated method stub
+		// if back key hit we do not run the spell checker
+	    if(e.getKeyCode() == e.VK_BACK_SPACE) {  
+	    	// do nothing
+	    } else {
+			if (Character.toString(e.getKeyChar()).matches("\\W")) { 
+				Thread runner = new Thread() {
+		
+					public void run() {				
+						// find the last non word char and split from there 
+			        	String temp = typingArea.getText();
+			        	CURRENT_MAX_WORD = temp.length() < MAX_SIZE_WORD ? temp.length() : CURRENT_MAX_WORD;
+
+			        	temp = temp.substring(temp.length() - CURRENT_MAX_WORD);
+			        	String[] parts = temp.split(" ");
+			        	
+			        	String lastWord = parts[parts.length - 1];
+			        	lastWord = lastWord.toLowerCase();
+			        	
+			        	if(!d.isWord(lastWord)) {
+			        		highlightManager.highlight(typingArea.getText().lastIndexOf(lastWord), lastWord.length());
+			        	}
+					}
+				};
+				runner.start();
+			} 
+	    }
 	}
 
 	@Override
 	public void keyReleased(KeyEvent e) {
 		// TODO Auto-generated method stub
+		
 	}
 
 	@Override
 	public void keyTyped(KeyEvent e) {
 		
-		// TODO Auto-generated method stub
-		if (Character.toString(e.getKeyChar()).matches("\\W")) { 
-			// find the last non word char and split from there 
-        	String temp = typingArea.getText();
-        	String[] parts = temp.split(" ");
-        	
-        	// String.valueOf(typingArea.getText().matches("\\W"));
-        	String lastWord = parts[parts.length - 1];
-        	lastWord = lastWord.toLowerCase();
-        	
-    		System.out.println(" To lower case: " + lastWord);
-        	System.out.println("\n word is: " + d.isWord(lastWord)); // "sentence"
-        	
-        	if(!d.isWord(lastWord)) {
-        		highlightManager.highlight(0, lastWord.length());
-        	}
-        	
-			if (d.isWord(lastWord)) {
-				System.out.println("\n WORDDDD Exists"); // "sentence"
-			}
-		}
 	}
 }
