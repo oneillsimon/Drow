22fadad79ebf2856b84ee5a3fd54a9ff0701561c
oneillsimon <oneill.simon92@gmail.com>
2014-03-16 16:08:31 +0000

    Updated alternative dictionary listener
    MyDictionaryListener now handles deletions correctly. I also fixed a bug
    in the highlighter.

--- src/drow/spellchecker/MyDictionaryListener.java
+++ src/drow/spellchecker/MyDictionaryListener.java
@@ -2,24 +2,32 @@ package drow.spellchecker;
 
 import java.awt.Color;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.StringTokenizer;
 
 import javax.swing.JTextPane;
+import javax.swing.event.CaretEvent;
+import javax.swing.event.CaretListener;
 import javax.swing.event.DocumentEvent;
 import javax.swing.event.DocumentListener;
 import javax.swing.text.BadLocationException;
 
 import drow.highlighter.DrowHighlightManager;
 
-public class MyDictionaryListener implements DocumentListener {
+public class MyDictionaryListener implements DocumentListener, CaretListener {
 
 	private JTextPane textPane;
 	private Dictionary dictionary;
 	private DrowHighlightManager highlightManager;
-	private String word = "";
-	boolean wordFinished = false;
+	private ArrayList<String> words;
+	private ArrayList<Integer> wordOffsets;
+	private String word;
+	private String wordAtCaret;
+	private boolean wordFinished;
 	
 	public MyDictionaryListener(JTextPane textPane) {
 		this.textPane = textPane;
+		this.textPane.addCaretListener(this);
 		
 		try {
 			dictionary = new Dictionary();
@@ -28,6 +36,11 @@ public class MyDictionaryListener implements DocumentListener {
 		}
 		
 		highlightManager = new DrowHighlightManager(textPane, Color.red);
+		words = new ArrayList<String>();
+		wordOffsets = new ArrayList<Integer>();
+		word = "";
+		wordAtCaret = "";
+		wordFinished = false;
 	}
 	
 	@Override
@@ -36,9 +49,37 @@ public class MyDictionaryListener implements DocumentListener {
 
 	@Override
 	public void insertUpdate(DocumentEvent e) {
+		determineWord(e, false);
+		cleanLists();
+	}
+
+	@Override
+	public void removeUpdate(DocumentEvent e) {
+		String wordBeingChanged = wordAtCaret;
+
+		for(int i = 0; i < words.size(); i++) {
+			if(words.get(i).equals(wordBeingChanged)) {
+				words.remove(words.get(i));
+				wordOffsets.remove(wordOffsets.get(i));
+			}
+		}
 		
+		determineWord(e, true);
+		cleanLists();
+	}
+
+	@Override
+	public void caretUpdate(CaretEvent e) {
+		for(int i = 0; i < words.size(); i++) {
+			if(e.getDot() >= wordOffsets.get(i) && e.getDot() <= wordOffsets.get(i) + words.get(i).length()) {
+				wordAtCaret = words.get(i);
+				System.out.println("Caret is at word: " + wordAtCaret);
+			}
+		}
+	}
+	
+	private void determineWord(DocumentEvent e, boolean removing) {
 		try {
-			
 			if(e.getDocument().getText(e.getOffset(), 1).equals(" ")) {
 				word = word.toLowerCase();
 				word = word.trim();
@@ -46,16 +87,29 @@ public class MyDictionaryListener implements DocumentListener {
 				if(!dictionary.isWord(word) && !word.equals(" ")) {
 					highlightManager.highlight(e.getOffset() - word.length(), word.length());
 				}
+				words.add(word);
+				wordOffsets.add(e.getOffset() - word.length());
 				wordFinished = true;
+				word = "";
 				
 			} else {
 				
-				if(!wordFinished) {
-					word += e.getDocument().getText(e.getOffset(), 1);
+				if (!removing) {
+					if (!wordFinished) {
+						word += e.getDocument().getText(e.getOffset(), 1);
+					} else {
+						word = "";
+						word += e.getDocument().getText(e.getOffset(), 1);
+						wordFinished = false;
+					}
 				} else {
-					word = "";
-					word += e.getDocument().getText(e.getOffset(), 1);
-					wordFinished = false;
+					if (!wordFinished) {
+						word = word.substring(0, word.length() - 1);
+					} else {
+						word = "";
+						word = word.substring(0, word.length() - 1);
+						wordFinished = false;
+					}
 				}
 			}
 			
@@ -63,9 +117,17 @@ public class MyDictionaryListener implements DocumentListener {
 			e1.printStackTrace();
 		}
 	}
-
-	@Override
-	public void removeUpdate(DocumentEvent e) {
+	
+	private void cleanLists() {
+		for(int i = 0; i < words.size(); i++) {
+			if(words.get(i).equals("")) {
+				words.remove(words.get(i));
+				wordOffsets.remove(wordOffsets.get(i));
+			}
+		}
 	}
-
+	
+	public String getWordAtCaret() {
+		return wordAtCaret;
+	}
 }
