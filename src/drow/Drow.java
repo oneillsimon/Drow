8c2ff31bee5a6180d21cf2e131fc66abd02f762e
oneillsimon <oneill.simon92@gmail.com>
2014-04-27 16:27:25 +0100

    Moved and renamed MainComponent

--- src/drow/Drow.java
+++ src/drow/Drow.java
@@ -1,14 +1,13 @@
+package drow;
+
 import javax.swing.UIManager;
 
 import drow.view.DocumentView;
 
-public class MainComponent {
+public class Drow {
 	public static void main(String[] args) {
 		try {
-			//UIManager.setLookAndFeel("com.jtattoo.plaf.noire.NoireLookAndFeel");
 			UIManager.setLookAndFeel("com.jtattoo.plaf.graphite.GraphiteLookAndFeel");
-			//UIManager.setLookAndFeel("com.jtattoo.plaf.hifi.HiFiLookAndFeel");
-			//UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
 		} catch (Exception e) {
 			System.err.println("Look and feel not set.");
 			System.out.println(e.getStackTrace() + "\n");
