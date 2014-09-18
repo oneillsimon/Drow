7478ef92da8610b8b285175ec66a15e11a1cbaa3
oneillsimon <oneill.simon92@gmail.com>
2014-09-18 16:39:59 +0100

    Added comments to DicitonaryListener

--- src/drow/spellchecker/DictionaryListener.java
+++ src/drow/spellchecker/DictionaryListener.java
@@ -16,29 +16,63 @@ import drow.styles.DrowStyles;
 
 /**
  * <h1>DictionaryListener</h1>
+ * <p>
+ * This class is a listener that monitors a JTextPane and compares 
+ * entered strings to words contained in a dictionary. If no match is found it will 
+ * underline the word, indicating a spelling mistake. If Drow is in Developer mode 
+ * language specific keywords will be highlighted using the same method.
+ * </p>
  * @author Lee Mc Donald
  * @author Simon O'Neill
  */
 public class DictionaryListener implements DocumentListener, CaretListener {
 
+	/** The dictionary containing the words. */
 	private Dictionary dictionary;
+	
+	/** The highlightManager takes care of underlining the text. */
 	private DrowHighlightManager highlightManager;
+	
+	/** The actionManager for styling the text. */
 	private DrowStyleActionManager actionManager;
+	
+	/** Stores the String found at the caret's position. */
 	private String wordAtCaret;
+	
+	/** String to determine if the word should be highlighted or underlined. */
 	private String highlightWhat;
+	
+	/** The textPane being listened to. */
 	private JTextPane textPane;
 	
+	/** The position of the caret. */
 	private int dot;
 	
+	/** The start position in the textPane of the word in question. */
 	int wordStart = -1;
+	
+	/** The end position in the textPane of the word in question. */
 	int wordEnd = -1;
 	
+	/** The event performed on the document. */
 	DocumentEvent docEvent;
 	
+	/**
+	 * <h1>Constructor</h1>
+	 * @param textPane - the JTextPane to listen to.
+	 * @param fileName - The file name of the dictionary to query.
+	 */
 	public DictionaryListener(JTextPane textPane, String fileName) {
 		this(textPane, fileName, "underline", Color.red);
 	}
 	
+	/**
+	 * <h1>Constructor</h1>
+	 * @param textPane - the JTextPane to listen to.
+	 * @param fileName - The file name of the dictionary to query.
+	 * @param highlightWhat - What to highlight, the word or and underline.
+	 * @param highlightColor - The colour of the highlight
+	 */
 	public DictionaryListener(JTextPane textPane, String fileName, String highlightWhat, Color highlightColor) {
 		textPane.addCaretListener(this);
 		
@@ -56,6 +90,11 @@ public class DictionaryListener implements DocumentListener, CaretListener {
 		this.textPane = textPane;
 	}
 	
+	/**
+	 * Paints the word using highlightManager.getColour().
+	 * @param offset - The position of the word in the JTextPane.
+	 * @param length - The length of the word.
+	 */
 	void paintWord(int offset, int length) {
 	    class OneShotTask implements Runnable {
 	        int offset;
@@ -77,6 +116,10 @@ public class DictionaryListener implements DocumentListener, CaretListener {
 	public void changedUpdate(DocumentEvent e) {
 	}
 
+	/**
+	 * Method called when something is inserted in the JTextPane.
+	 * @param e - The document event.
+	 */
 	@Override
 	public void insertUpdate(DocumentEvent e) {
 		
@@ -144,15 +187,27 @@ public class DictionaryListener implements DocumentListener, CaretListener {
 	public void removeUpdate(DocumentEvent e) {
 	}
 	
+	/**
+	 * Method to keep the position of the caret updated.
+	 * @param e - The caret event.
+	 */
 	@Override
 	public void caretUpdate(CaretEvent e) {
 		dot = e.getDot();
 	}
 	
+	/**
+	 * Gets the word at the caret's position.
+	 * @return String - The word at the caret's position.
+	 */
 	public String getWordAtCaret() {
 		return wordAtCaret;
 	}
 	
+	/**
+	 * Gets the dictionary.
+	 * @return Dictionary - The Dictionary containing the words to check against.
+	 */
 	public Dictionary getDictionary() {
 		return dictionary;
 	}
